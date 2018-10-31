f8a27ffb0ba50549b0b1c10df1b7e171e642a0c1
Leslie Osorio <lesosorio17@gmail.com>
2018-10-31 21:57:22 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,10 @@ integer = [3,5,7,9]
 
 def my_each(integer)
   while integer < 10 do |i|
-    yield 
+    yield
   end
 end
 
-my_each(integer) do |i|
-  puts "i"
-end
+# my_each(integer) do |i|
+#   puts "i"
+# end
